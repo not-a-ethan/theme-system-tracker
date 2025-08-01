723c319a0f5b7b2e4ebf5cd630a57d497a6e5cdf
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-08-01 08:07:58 -0400

    started working on modals, commiting to transfer code to my laptop

--- app/manage-theme/page.tsx
+++ app/manage-theme/page.tsx
@@ -6,6 +6,11 @@ import { useSession } from "next-auth/react";
 
 import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@heroui/table";
 import { Tooltip } from "@heroui/tooltip";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@heroui/modal";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Checkbox } from "@heroui/checkbox";
+import {Link} from "@heroui/link";
 
 import { EditIcon } from "./icons";
 import { DeleteIcon } from "./icons";
@@ -15,6 +20,8 @@ import styles from "../../styles/manageTheme.module.css"
 export default function manageTheme() {
     const { data: session, status } = useSession();
     const router = useRouter();
+
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
     if (status === "loading") {
         return (
@@ -48,11 +55,14 @@ export default function manageTheme() {
                         <TableCell>Jan 1 1970</TableCell>
                         <TableCell>
                             <div className={styles.iconsDiv}>
-                                <Tooltip content="Edit theme">
-                                    <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                        <EditIcon />
-                                    </span>
-                                </Tooltip>
+                                <Button onPress={onOpen}>
+                                    <Tooltip content="Edit theme">
+                                        <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                            <EditIcon />
+                                        </span>
+                                    </Tooltip>
+                                </Button>
+                                
                                 <Tooltip color="danger" content="Delete theme">
                                     <span className="text-lg text-danger cursor-pointer active:opacity-50">
                                         <DeleteIcon />
@@ -81,6 +91,49 @@ export default function manageTheme() {
                     </TableRow>
                 </TableBody>
             </Table>
+
+            <Modal isOpen={isOpen} placement="center" onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                        <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+                        <ModalBody>
+                            <Input
+                            label="Email"
+                            placeholder="Enter your email"
+                            variant="bordered"
+                            />
+                            <Input
+                            label="Password"
+                            placeholder="Enter your password"
+                            type="password"
+                            variant="bordered"
+                            />
+                            <div className="flex py-2 px-1 justify-between">
+                            <Checkbox
+                                classNames={{
+                                label: "text-small",
+                                }}
+                            >
+                                Remember me
+                            </Checkbox>
+                            <Link color="primary" href="#" size="sm">
+                                Forgot password?
+                            </Link>
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="danger" variant="flat" onPress={onClose}>
+                            Close
+                            </Button>
+                            <Button color="primary" onPress={onClose}>
+                            Sign in
+                            </Button>
+                        </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </>
     )
 }
